85f3a0663d6b0cc24380ebe78f695b7f59a05749
Jesus Guerrero <guerrero.jec@gmail.com>
2025-11-05 15:04:15 -0700

    fixed automapper

--- Backend/API/e.Pizza.API/Program.cs
+++ Backend/API/e.Pizza.API/Program.cs
@@ -1,5 +1,6 @@
 using ePizza.Application;
 using ePizza.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 namespace ePizza.API
 {
     public class Program
@@ -19,12 +20,27 @@ namespace ePizza.API
             builder.Services.AddSwaggerGen();
 
             // added reference of Application Layer
-            builder.Services.AddAutoMapper(typeof(ApplicationAssemblyMarker).Assembly);
+            // builder.Services.AddAutoMapper()
+            builder.Services.AddAutoMapper(cfg =>
+             {
+                // ⚠️ Required for AutoMapper 15+ if you are using the commercial/OSS dual license.
+                // Replace <License Key Here> with your actual key if required.
+                // If you are using it under the OSS license, this configuration block is still required.
+                // cfg.LicenseKey = "<License Key Here>";
+
+             }, typeof(ApplicationAssemblyMarker).Assembly);
             builder.Services.AddApplication();
 
 
             // adding reference of Infra layer
-            builder.Services.AddAutoMapper(typeof(InfraAssemblyMarker).Assembly);
+             builder.Services.AddAutoMapper(cfg =>
+             {
+                // ⚠️ Required for AutoMapper 15+ if you are using the commercial/OSS dual license.
+                // Replace <License Key Here> with your actual key if required.
+                // If you are using it under the OSS license, this configuration block is still required.
+                // cfg.LicenseKey = "<License Key Here>";
+
+             }, typeof(InfraAssemblyMarker).Assembly);
             builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("DatabaseConnection")!);
 
 
